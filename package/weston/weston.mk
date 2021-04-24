72805ad1eed7db857c361f2edee5351de5cf1b70
Jeffy Chen <jeffy.chen@rock-chips.com>
2021-04-25 06:49:28 +0800

    weston: Use pixman renderer by default for mesa3d
    The gl-renderer is too slow in this case.
    
    Change-Id: Ief3ace90f7decabaf2cc44b24f95c424bb0eb06c
    Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>

--- package/weston/weston.mk
+++ package/weston/weston.mk
@@ -138,6 +138,15 @@ else
 WESTON_CONF_OPTS += -Ddemo-clients=false
 endif
 
+ifeq ($(BR2_PACKAGE_WESTON_DEFAULT_PIXMAN),y)
+define WESTON_INSTALL_PIXMAN_INI
+        $(INSTALL) -D -m 0644 $(WESTON_PKGDIR)/pixman.ini \
+                $(TARGET_DIR)/etc/weston/weston.ini.d/01-pixman.ini
+endef
+
+WESTON_POST_INSTALL_TARGET_HOOKS += WESTON_INSTALL_PIXMAN_INI
+endif
+
 define WESTON_INSTALL_TARGET_ENV
         $(INSTALL) -D -m 0644 $(WESTON_PKGDIR)/weston.sh \
                 $(TARGET_DIR)/etc/profile.d/weston.sh
