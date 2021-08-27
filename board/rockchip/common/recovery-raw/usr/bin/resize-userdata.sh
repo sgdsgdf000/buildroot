1b8a21923227d3d7b81a21cc351e6be8f8d101ab
liulq <llq@t-chip.com.cn>
2021-08-27 16:15:07 +0800

    overlay: common: add recovery-raw

---
+++ board/rockchip/common/recovery-raw/usr/bin/resize-userdata.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+block_name=$1
+device=$(udevadm info --query=path --name=${block_name})
+block_num=${device##*[a-zA-Z]}
+device="/dev/$(echo ${device} | awk -F'/' '{print $(NF-1)}')"
+
+/usr/sbin/sgdisk -e ${device}
+/usr/sbin/parted ${device} <<EOF
+resizepart ${block_num} -34s
+q
+EOF
+/sbin/mke2fs -t ext4 -b 4096 -O ^huge_file -m 0 -q -F ${block_name}
+/sbin/e2fsck -fy ${block_name}
