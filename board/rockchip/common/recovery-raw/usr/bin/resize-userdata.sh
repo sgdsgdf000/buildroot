ea59b7bbab3d02fb4303b770242b0e301e797bb9
liulq <llq@t-chip.com.cn>
2021-09-03 17:01:40 +0800

    overlay: common: update resize-userdata.sh
    Supoort multi media boot.
    
    Use storagenode to distinguish multiple boot media of the same type.

--- board/rockchip/common/recovery-raw/usr/bin/resize-userdata.sh
+++ board/rockchip/common/recovery-raw/usr/bin/resize-userdata.sh
@@ -1,9 +1,62 @@
 #!/bin/sh
 
-block_name=$1
+resolve_device() {
+	local BOOTMEDIA=""
+	local BOOTDEVICE="/dev"
+	local BOOTNODE=""
+	local NODENAME=""
+	local NODEADDR=""
+
+	for x in $(cat /proc/cmdline); do
+			case ${x} in
+				storagemedia=*)
+					BOOTMEDIA=$(echo ${x} | cut -f 2 -d =)
+				;;
+				storagenode=*)
+					BOOTNODE=$(echo ${x} | sed 's/\///g' | cut -f 2 -d =)
+					NODENAME=$(echo "$BOOTNODE" | cut -f 1 -d @)
+					NODEADDR=$(echo "$BOOTNODE" | cut -f 2 -d @)
+					BOOTNODE="$NODEADDR.$NODENAME"
+				;;
+			esac
+	done
+
+	case ${BOOTMEDIA} in
+		emmc | sd)
+			BOOTDEVICE="/dev/mmcblk"
+			;;
+		usb | scsi)
+			BOOTDEVICE="/dev/sd"
+			;;
+	esac
+
+	DEV="$(ls ${BOOTDEVICE}*${block_num})"
+	for x in $(echo "$DEV"); do
+		udevadm info --query=path --name=${x} | grep -q "$BOOTNODE"
+		if [[ $? = 0 ]]; then
+			block_name=${x}
+			break
+		fi
+	done
+
+	case ${BOOTMEDIA} in
+		emmc | sd)
+			device=${block_name%[a-zA-Z]*}
+			;;
+		usb | scsi)
+			device=${block_name%[0-9]*}
+			;;
+	esac
+}
+
+if [[ -z $1 ]]; then
+	block_name="/dev/block/by-name/userdata"
+else
+	block_name=$1
+fi
 device=$(udevadm info --query=path --name=${block_name})
 block_num=${device##*[a-zA-Z]}
-device="/dev/$(echo ${device} | awk -F'/' '{print $(NF-1)}')"
+resolve_device
 
 /usr/sbin/sgdisk -e ${device}
 /usr/sbin/parted ${device} <<EOF
