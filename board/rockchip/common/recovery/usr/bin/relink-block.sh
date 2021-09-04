6db5cf5b34a5f47acc1fc9d8faf98b25fa32f0f7
liulq <llq@t-chip.com.cn>
2021-09-04 16:30:08 +0800

    overlay: common: add relink-block.sh
    We need /dev/block/by-name/ to link to the corresponding boot media.

---
+++ board/rockchip/common/recovery/usr/bin/relink-block.sh
@@ -0,0 +1,75 @@
+#!/bin/sh
+
+cat /proc/cmdline |grep -q "storagenode"
+if [[ $? != 0 ]]; then
+	exit 1
+fi
+
+wait_for_file() {
+	file="$1"
+	timeout="$2"
+	local count
+
+	[ "$timeout" == "0" ] && return
+
+	count=0
+	while [ -z "$(ls $file)" ]; do
+		sleep 1
+		count=$((count + 1))
+		[ "$count" == "$timeout" ] && break
+	done
+}
+
+relink_block() {
+	local BOOTMEDIA=""
+	local BOOTDEVICE="/dev"
+	local BOOTNODE=""
+	local NODENAME=""
+	local NODEADDR=""
+	local device=""
+	local block_name=""
+
+	for x in $(cat /proc/cmdline); do
+		case ${x} in
+			storagemedia=*)
+				BOOTMEDIA=$(echo ${x} | cut -f 2 -d =)
+			;;
+			storagenode=*)
+				BOOTNODE=$(echo ${x} | sed 's/\///g' | cut -f 2 -d =)
+				NODENAME=$(echo "$BOOTNODE" | cut -f 1 -d @)
+				NODEADDR=$(echo "$BOOTNODE" | cut -f 2 -d @)
+				BOOTNODE="$NODEADDR.$NODENAME"
+			;;
+		esac
+	done
+
+	case ${BOOTMEDIA} in
+		emmc | sd)
+			wait_for_file /dev/mmcblk* 10
+			BOOTDEVICE=$(ls /dev/mmcblk[0-9])
+			;;
+		usb | scsi)
+			wait_for_file /dev/sd* 10
+			BOOTDEVICE=$(ls /dev/sd[a-z])
+			;;
+	esac
+
+	for x in $(echo "$BOOTDEVICE"); do
+		udevadm info --query=path --name=${x} | grep -q "$BOOTNODE"
+		if [[ $? = 0 ]]; then
+			device=${x}
+			break
+		fi
+	done
+
+	for x in $(ls ${device}*); do
+		block_name=$(udevadm info --name=${x} | grep "ID_PART_ENTRY_NAME" | cut -f 2 -d =)
+		if [[ -n "$block_name" ]]; then
+			ln -sf ${x} /dev/block/by-name/${block_name}
+		fi
+	done
+}
+
+relink_block
+
+exit 0
