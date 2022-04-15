4dc95a4b45fed8215344678946e9a3dc75c681f4
lvsx <lvsx@t-chip.com.cn>
2022-04-15 15:59:48 +0800

    package: stressapptest: fix download error

--- package/stressapptest/stressapptest.mk
+++ package/stressapptest/stressapptest.mk
@@ -3,9 +3,10 @@
 # stress app test
 #
 ################################################################################
-STRESSAPPTEST_SITE = https://github.com/stressapptest/stressapptest.git
+STRESSAPPTEST_SITE = https://github.com/stressapptest/stressapptest
 STRESS_LICENSE = Apache-2.0
 STRESS_LICENSE_FILES = COPYING
-STRESSAPPTEST_VERSION = master
+STRESSAPPTEST_VERSION = 6714c57d0d67f5a2a7a9987791af6729289bf64e
+STRESSAPPTEST_SITE_METHOD = git
 
 $(eval $(autotools-package))
