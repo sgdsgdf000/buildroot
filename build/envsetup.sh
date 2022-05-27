8e428b754ca1278d6e75333f628cfbbbd5138074
lvsx <lvsx@t-chip.com.cn>
2022-05-27 09:48:47 +0800

    build: add firefly defconfig support

--- build/envsetup.sh
+++ build/envsetup.sh
@@ -78,7 +78,7 @@ function main()
 	echo Top of tree: ${TOP_DIR}
 
 	RK_DEFCONFIG_ARRAY=(
-		$(cd ${BUILDROOT_DIR}/configs/; ls rockchip_* | \
+		$(cd ${BUILDROOT_DIR}/configs/; ls rockchip_* firefly_* | \
 			sed "s/_defconfig$//" | grep "$1" | sort)
 	)
 
